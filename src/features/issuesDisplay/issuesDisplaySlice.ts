641814c50118e243b8a3097b323f5693bd43337d
aukoda <kodagiwa@gmail.com>
2020-11-08 18:56:37 +0100

    added reducer for issues display

--- src/features/issuesDisplay/issuesDisplaySlice.ts
+++ src/features/issuesDisplay/issuesDisplaySlice.ts
@@ -15,10 +15,38 @@ interface CurrentRepo {
   repo: string 
 }
 
-interface CurrentDisplayState = {
+type CurrentDisplayState = {
   page: number
-} & CurrentDisplay & CurrentDisplay
+} & CurrentDisplay & CurrentRepo
 
-const issuesDisplaySlice = createSlice({});
+let initialState: CurrentDisplayState = {
+  org: "rails",
+  repo: "rails",
+  page: 1,
+  displayType: "issues",
+  issueId: null
+}
+
+const issuesDisplaySlice = createSlice({
+  name: "issuesDisplay",
+  initialState,
+  reducers: {
+    displayRepo(state, action: PayloadAction<CurrentRepo>) {
+      const { org, repo } = action.payload;
+      state.org = org 
+      state.repo = repo
+    }, 
+    setCurrentPage(state, action: PayloadAction<number>) {
+      state.page = action.payload
+    },
+    setCurrentDisplayType(state, action: PayloadAction<CurrentDisplayPayload>) {
+      const { displayType, issueId = null } = action.payload
+      state.displayType = displayType
+      state.issueId = issueId
+    }
+  }
+});
+
+export const { displayRepo, setCurrentDisplayType, setCurrentPage } = issuesDisplaySlice.actions
 
 export default issuesDisplaySlice.reducer
